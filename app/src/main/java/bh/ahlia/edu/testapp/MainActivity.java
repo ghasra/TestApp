063d2d983f672e9b826c1f78752c36f960070ac1
ghasra <33757320+ghasra@users.noreply.github.com>
2019-06-18 14:06:56 +0300

    Initial commit

---
+++ app/src/main/java/bh/ahlia/edu/testapp/MainActivity.java
@@ -0,0 +1,14 @@
+package bh.ahlia.edu.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
